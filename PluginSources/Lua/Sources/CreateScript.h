d7124215d05b7435d86bd23bbcca4ebbdf160172
Lobsterzelda <tersaderwaslink@gmail.com>
2025-02-27 02:09:28 -0700

    Updated Lua Plugin

--- PluginSources/Lua/Sources/CreateScript.h
+++ PluginSources/Lua/Sources/CreateScript.h
@@ -1,17 +1,20 @@
-#ifndef CREATE_SCRIPT_H
-#define CREATE_SCRIPT_H
+#pragma once
 
 #include "DolphinDefinedAPIs.h"
 
+namespace Scripting
+{
+
 #ifdef __cplusplus
-extern "C" {
+  extern "C" {
 #endif
 
 
-DLL_Export void* CreateNewScript(int, const char*, Dolphin_Defined_ScriptContext_APIs::PRINT_CALLBACK_FUNCTION_TYPE, Dolphin_Defined_ScriptContext_APIs::SCRIPT_END_CALLBACK_FUNCTION_TYPE);
+    DLL_Export void* CreateNewScript(int, const char*, Dolphin_Defined_ScriptContext_APIs::PRINT_CALLBACK_FUNCTION_TYPE, Dolphin_Defined_ScriptContext_APIs::SCRIPT_END_CALLBACK_FUNCTION_TYPE);
 
 #ifdef __cplusplus
+  }
+#endif
+
 }
-#endif
 
-#endif
--- PluginSources/Python/Sources/CopiedScriptContextFiles/InternalScriptAPIs/FileUtility_APIs.h
+++ PluginSources/Python/Sources/CopiedScriptContextFiles/InternalScriptAPIs/FileUtility_APIs.h
@@ -1,19 +1,26 @@
 #pragma once
 
+namespace Scripting
+{
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+// FileUtility_APIs contains the APIs for getting the user directory path and the system directory
+// path for Dolphin.
 typedef struct FileUtility_APIs
 {
   // Returns the user directory path for Dolphin.
-  const char* (*GetUserPath)();
+  const char* (*GetUserDirectoryPath)();
 
   // Returns the system directory path for Dolphin.
-  const char* (*GetSystemDirectory)();
+  const char* (*GetSystemDirectoryPath)();
 
 } FileUtility_APIs;
 
 #ifdef __cplusplus
 }
 #endif
+
+}  // namespace Scripting
